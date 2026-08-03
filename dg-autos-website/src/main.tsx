a6d9a2e8a8f34d74b3356e2899020455680c68e4
galloxxv <d.g.autossalesgroup@gmail.com>
2026-08-02 23:26:21 -0500

    Route secure estimate links to approval portal

--- dg-autos-website/src/main.tsx
+++ dg-autos-website/src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './finishing.css'
 import './finance.css'
+import './portal.css'
 import App from './App.tsx'
+import ApprovalPortal from './ApprovalPortal.tsx'
+
+const token = new URLSearchParams(window.location.search).get('estimate')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {token ? <ApprovalPortal token={token} /> : <App />}
   </StrictMode>,
 )
